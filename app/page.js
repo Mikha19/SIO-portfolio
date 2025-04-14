45fe10d610c7f824ed7ff01e4a806b0fae339aac
Mikhael <alexandremikhael1911@gmail.com>
2025-04-14 16:11:15 +0200

    change

--- app/page.js
+++ app/page.js
@@ -1,6 +1,6 @@
 export default function Home() {
     return (
-        <div className="bg-amber-400 text-4xl">
+        <div className="size-full text-4xl">
             Ce site est en développement. <br />
             Merci de votre compréhension.
         </div>
