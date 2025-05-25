f478f8cc26926cb8de5331ba924bd285a703e0d2
Mikha19 <alexandremikhael1911@gmail.com>
2025-05-25 12:37:29 +0200

    fix

--- app/components/data/infoCardData.js
+++ app/components/data/infoCardData.js
@@ -2,7 +2,7 @@ export const informations = [
     {
         id: 1,
         title: "Moi",
-        image: "/self.png",
+        image: "/images/informations/self.png",
         description: "Jeune apprenti en BTS Service Informatique aux Organisations, option Solutions Logicielles et Applications Métiers.",
         text: "Je suis passionné par le développement informatique, et je m'efforce d'apprendre chaque jour pour réaliser mon but qui est de crée des jeux vidéos.",
         imageClass: "rounded-full"
@@ -10,7 +10,7 @@ export const informations = [
     {
         id: 2,
         title: "Entreprise",
-        image: "/images/owlcubLogo.png",
+        image: "/images/informations/owlcubLogo.png",
         description: "Owlcub, une plateforme centralisée pour surveiller, anticiper et protéger, avec une multitude d'intégrations.",
         text: "Owlcub est une startup innovante qui propose une solution de cybersécurité unique basée sur l'IA. Elle permet de surveiller, anticiper et protéger les entreprises contre les menaces informatiques. Grâce à une multitude d'intégrations, elle centralise toutes les alertes de sécurité en un seul endroit, facilitant ainsi la gestion des incidents.",
         imageClass: "rounded-lg"
@@ -18,7 +18,7 @@ export const informations = [
     {
         id: 3,
         title: "Centre de formation",
-        image: "/images/campusErmitage.png",
+        image: "/images/informations/campusErmitage.png",
         description: "Le Campus ERMITAGE, centre de formation proposant des formations allant du BAC+2 au BAC+3, grâce à l'alternance.",
         text: "Le Campus ERMITAGE est un ",
         imageClass: "rounded-full"
