1a1d783985670a5d15aceee93fe58fdca8953a37
Mikha19 <alexandremikhael1911@gmail.com>
2025-10-08 16:07:57 +0200

    fix image presentation not loading

--- app/components/data/presCardData.js
+++ app/components/data/presCardData.js
@@ -2,7 +2,7 @@ export const presentations = [
     {
         id: 1,
         title: "Moi",
-        image: "/images/informations/self.png",
+        image: "/images/presentation/self.png",
         description: "Jeune apprenti en BTS Service Informatique aux Organisations, option Solutions Logicielles et Applications Métiers.",
         text: "Je suis passionné par le développement informatique, et je m'efforce d'apprendre chaque jour pour réaliser mon but qui est de crée des jeux vidéos. Je travaille actuellement en alternance dans une startup innovante, où je mets en pratique mes compétences en programmation pour réaliser mes objectifs.",
         imageClass: "rounded-full"
@@ -10,7 +10,7 @@ export const presentations = [
     {
         id: 2,
         title: "Entreprise",
-        image: "/images/informations/OwlcubLogo.png",
+        image: "/images/presentation/OwlcubLogo.png",
         description: "Owlcub, une plateforme centralisée pour surveiller, anticiper et protéger, avec une multitude d'intégrations.",
         text: "Owlcub est une startup innovante qui propose une solution de cybersécurité unique basée sur l'IA. Elle permet de surveiller, anticiper et protéger les entreprises contre les menaces informatiques. Grâce à une multitude d'intégrations, elle centralise toutes les alertes de sécurité en un seul endroit, facilitant ainsi la gestion des incidents.",
         imageClass: "rounded-lg"
@@ -18,7 +18,7 @@ export const presentations = [
     {
         id: 3,
         title: "Centre de formation",
-        image: "/images/informations/campusErmitage.png",
+        image: "/images/presentation/campusErmitage.png",
         description: "Le Campus ERMITAGE, centre de formation proposant des formations allant du BAC+2 au BAC+3, grâce à l'alternance.",
         text: "Le Campus Ermitage Sup’, situé à Agen (Lot-et-Garonne), est un établissement d’enseignement supérieur privé qui propose des formations post-bac en initial ou en alternance. Il fait partie du réseau CNEAP et est relié au lycée agricole privé l’Ermitage. Le campus propose des formations allant du BAC+2 au BAC+3 dans divers domaines tels que le commerce, le marketing, la gestion, l’informatique et les ressources humaines.",
         imageClass: "rounded-full"
